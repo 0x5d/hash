2e52b2b497ea2f265882c9a39d6bf1463321f32e
Juan Castillo <castillobgr@gmail.com>
2024-12-14 15:11:37 -0500

    api/http: Add URL router

--- api/http/http.go
+++ api/http/http.go
@@ -10,13 +10,15 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/0x5d/hash/core"
 	"go.uber.org/zap"
 )
 
 // Server is an HTTP server.
 type Server struct {
-	c   Config
-	Log *zap.Logger
+	c         Config
+	shortener core.Shortener
+	log       *zap.Logger
 }
 
 // Config holds an HTTP server's configuration.
@@ -31,8 +33,8 @@ func DefaultConfig() Config {
 }
 
 // NewServer returns a new Server instance configured with the given Config.
-func NewServer(c Config, log *zap.Logger) *Server {
-	return &Server{c, log}
+func NewServer(c Config, shortener core.Shortener, log *zap.Logger) *Server {
+	return &Server{c: c, shortener: shortener, log: log}
 }
 
 // Start starts the server. It handles shutdown gracefully by not accepting new connections after
@@ -42,12 +44,14 @@ func (s *Server) Start(ctx context.Context) {
 		Addr: s.c.Addr,
 	}
 
+	http.Handle("/url", &urlRouter{&s.shortener})
+
 	go func() {
 		err := server.ListenAndServe()
 		if !errors.Is(err, http.ErrServerClosed) {
-			s.Log.Fatal("HTTP server shutdown unexpectedly", zap.Error(err))
+			s.log.Fatal("HTTP server shutdown unexpectedly", zap.Error(err))
 		}
-		s.Log.Info("HTTP server stopped")
+		s.log.Info("HTTP server stopped")
 	}()
 
 	ch := make(chan os.Signal, 1)
@@ -57,9 +61,9 @@ func (s *Server) Start(ctx context.Context) {
 	shutdownCtx, cancel := context.WithTimeout(ctx, s.c.ShutdownTimeout)
 	defer cancel()
 
-	s.Log.Info("HTTP server Shutting down")
+	s.log.Info("HTTP server Shutting down")
 	err := server.Shutdown(shutdownCtx)
 	if err != nil {
-		s.Log.Fatal("HTTP server failed to shutdown", zap.Error(err))
+		s.log.Fatal("HTTP server failed to shutdown", zap.Error(err))
 	}
 }
---
+++ api/http/url.go
@@ -0,0 +1,118 @@
+package http
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"path"
+	"strconv"
+	"strings"
+
+	"github.com/0x5d/hash/core"
+)
+
+const (
+	// Firefox allows URLs up to 64k chars, so 2x that should be enough.
+	bodyLimit = 128000
+	slash     = "/"
+)
+
+type errResponse struct {
+	Msg string `json:"msg"`
+}
+
+type urlRouter struct {
+	shortener *core.Shortener
+}
+
+type urlBody struct {
+	URL string `json:"url"`
+}
+
+func (b *urlBody) toCoreURL() core.ShortenedURL {
+	return core.ShortenedURL{URL: b.URL}
+}
+
+func (r *urlRouter) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		r.handleGet(res, req)
+	case http.MethodPost:
+		r.handlePost(res, req)
+	case http.MethodPut:
+		r.handlePut(res, req)
+	default:
+		http.Error(res, fmt.Sprintf("%q isn't allowed on URL resources", req.Method), http.StatusMethodNotAllowed)
+	}
+}
+
+func (r *urlRouter) handleGet(res http.ResponseWriter, req *http.Request) {
+	idStr, _ := shiftPath(req.URL.Path)
+	_, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(res, "URL ID must be an integer", http.StatusBadRequest)
+		return
+	}
+}
+
+func (r *urlRouter) handlePost(res http.ResponseWriter, req *http.Request) {
+	var u urlBody
+	body := http.MaxBytesReader(res, req.Body, bodyLimit)
+	parseJSON(body, &u, res)
+
+	shortened, err := r.shortener.Shorten(u.URL)
+	if err != nil {
+		writeErrRes(res, "Failed to shorten URL", http.StatusInternalServerError)
+		return
+	}
+	res.WriteHeader(http.StatusCreated)
+	bs, err := json.Marshal(&shortened)
+	if err != nil {
+		writeErrRes(res, "Failed to encode response", http.StatusInternalServerError)
+	}
+	res.Write(bs)
+}
+
+func (r *urlRouter) handlePut(res http.ResponseWriter, req *http.Request) {
+	idStr, _ := shiftPath(req.URL.Path)
+	_, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(res, "URL ID must be an integer", http.StatusBadRequest)
+		return
+	}
+}
+
+func shiftPath(p string) (head, tail string) {
+	p = path.Clean(slash + p)
+	i := strings.Index(p[1:], slash) + 1
+	if i <= 0 {
+		return p[1:], slash
+	}
+	return p[1:i], p[i:]
+}
+
+func parseJSON(body io.ReadCloser, v any, res http.ResponseWriter) {
+	err := json.NewDecoder(body).Decode(&v)
+	if err == nil {
+		return
+	}
+
+	switch {
+	case errors.Is(err, &http.MaxBytesError{}):
+		writeErrRes(res, fmt.Sprintf("Request body should be under %dB", bodyLimit), http.StatusRequestEntityTooLarge)
+	default:
+		writeErrRes(res, "Invalid JSON", http.StatusBadRequest)
+	}
+}
+
+func writeErrRes(res http.ResponseWriter, msg string, status int) {
+	errRes := errResponse{Msg: msg}
+	r, err := json.Marshal(&errRes)
+	if err != nil {
+		http.Error(res, "Failed to encode response.", http.StatusInternalServerError)
+		return
+	}
+	http.Error(res, string(r), status)
+}
