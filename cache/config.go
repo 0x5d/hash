bbb1658e6e475f351906821546c21c9e15f5e198
Juan Castillo <castillobgr@gmail.com>
2024-12-15 13:30:07 -0500

    Add cache module, Redis impl

---
+++ cache/config.go
@@ -0,0 +1,19 @@
+package cache
+
+import "time"
+
+type Config struct {
+	Address      string        `env:"ADDR"`
+	Password     string        `env:"PASS"`
+	TTL          time.Duration `env:"DEFAULT_TTL"`
+	ReadTimeout  time.Duration `env:"READ_TIMEOUT"`
+	WriteTimeout time.Duration `env:"WRITE_TIMEOUT"`
+}
+
+func DefaultConfig() Config {
+	return Config{
+		TTL:          24 * time.Hour,
+		ReadTimeout:  100 * time.Millisecond,
+		WriteTimeout: 1 * time.Second,
+	}
+}
---
+++ cache/redis.go
@@ -0,0 +1,43 @@
+package cache
+
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisCache struct {
+	c  Config
+	cl *redis.Client
+}
+
+func NewRedisCache(ctx context.Context, c Config) (*RedisCache, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     c.Address,
+		Password: c.Password,
+	})
+	_, err := rdb.Get(ctx, "").Result()
+	if err != nil && err != redis.Nil {
+		return nil, err
+	}
+	return &RedisCache{c: c, cl: rdb}, nil
+}
+
+func (r *RedisCache) Set(ctx context.Context, key, url string) error {
+	ctx, cancel := context.WithTimeout(ctx, r.c.WriteTimeout)
+	defer cancel()
+	return r.cl.Set(ctx, key, url, r.c.TTL).Err()
+}
+
+func (r *RedisCache) Get(ctx context.Context, key string) (string, error) {
+	ctx, cancel := context.WithTimeout(ctx, r.c.ReadTimeout)
+	defer cancel()
+	url, err := r.cl.Get(ctx, key).Result()
+	if err == redis.Nil {
+		return "", nil
+	}
+	if err != nil {
+		return "", err
+	}
+	return url, nil
+}
