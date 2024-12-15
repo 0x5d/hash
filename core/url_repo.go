26ec99ab9942ad765e8df17ae1d51a1d1c1374bb
Juan Castillo <castillobgr@gmail.com>
2024-12-14 21:08:06 -0500

    core: Add repo interface

---
+++ core/url_repo.go
@@ -0,0 +1,11 @@
+package core
+
+import (
+	"context"
+)
+
+type URLRepository interface {
+	NextID(ctx context.Context) (uint64, error)
+	Create(ctx context.Context, url ShortenedURL) error
+	Update(ctx context.Context, id uint64, newURL *string, enabled *bool) error
+}
