d8741587d4a122fb1227b3a125f5f3b4e894b401
Juan Castillo <castillobgr@gmail.com>
2024-12-14 21:19:33 -0500

    pers: Add migrations

---
+++ persistence/migrations/001_create_schema.sql
@@ -0,0 +1,18 @@
+create table if not exists url_ids(
+  id serial primary key,
+  url_id bigint not null check (url_id >= 0)
+);
+
+insert into url_ids(id, url_id) values (default, 0);
+
+create table if not exists urls(
+  id serial primary key,
+  url varchar(2048) not null,
+  short_key varchar(2048) not null,
+  enabled boolean not null default true
+);
+
+---- create above / drop below ----
+
+drop table url_ids;
+drop table urls;
