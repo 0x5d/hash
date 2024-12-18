87636ac05abdf0ffd54b825ea5613c045c0aa612
Juan Castillo <castillobgr@gmail.com>
2024-12-17 21:38:58 -0500

    Move entrypoint so that it works better with ko

--- src/main.go
+++ src/main.go
@@ -19,13 +19,18 @@ func main() {
 	if err != nil {
 		logger.Fatal("Failed to load config from env", zap.Error(err))
 	}
-	urlRepo, err := persistence.NewPGURLRepo(ctx, c.DB)
+	urlRepo, err := persistence.NewPGURLRepo(logger, ctx, c.DB)
 	if err != nil {
 		logger.Fatal("Failed to start URL repo", zap.Error(err))
 	}
 	cache, err := cache.NewRedisCache(ctx, c.Cache)
 	if err != nil {
 		logger.Fatal("Failed to connect to cache", zap.Error(err))
+	}
+	logger.Info("Table", zap.String("table", c.DB.MigrationsTable))
+	err = urlRepo.Migrate(ctx)
+	if err != nil {
+		logger.Fatal("Failed to apply migrations", zap.Error(err))
 	}
 	urlSvc := core.NewURLService(logger, urlRepo, cache)
 	s := http.NewServer(c.HTTP, urlSvc, logger.Named("http"))
